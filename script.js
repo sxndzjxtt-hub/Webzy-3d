f9958f14265fe90b1c03abd3d5e4f7e9177a0deb
sxndzjxtt-hub <sxndzjxtt@gmail.com>
2026-03-28 05:17:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function generate() {
 
   let prompt = document.getElementById("prompt").value;
 
-  let res = await fetch("YOUR_BACKEND_URL/generate", {
+  let res = await fetch("webzy-3d-backend-production.up.railway.app/generate", {
     method:"POST",
     headers:{"Content-Type":"application/json"},
     body: JSON.stringify({prompt})
