bfbe81eb6ce65af303a098a1853b7d04a9411782
sxndzjxtt-hub <sxndzjxtt@gmail.com>
2026-03-28 05:38:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ async function generate() {
 
   let prompt = document.getElementById("prompt").value;
 
-  let res = await fetch("webzy-3d-backend-production.up.railway.app/generate", {
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body: JSON.stringify({prompt})
+  let res = await fetch("https://webzy-3d-backend-production.up.railway.app/generate", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ prompt: prompt })
   });
 
   let html = await res.text();
+
   document.getElementById("output").srcdoc = html;
 }
